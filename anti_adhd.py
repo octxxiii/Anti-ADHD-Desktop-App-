7ba475150949bf5c87304c04f3a103e67a77e3e8
MinJun Kim <kdyw123@gmail.com>
2025-04-30 01:07:58 +0900

    fix: 설정 창에 아이콘 적용

--- anti_adhd.py
+++ anti_adhd.py
@@ -216,6 +216,34 @@ class QuadrantChecklist:
         settings_window.transient(self.root)
         settings_window.grab_set()
         
+        # 설정 창에 아이콘 설정
+        try:
+            settings_window.iconbitmap("app_icon.ico")
+        except:
+            try:
+                # 임시 아이콘 생성
+                img = Image.new('RGBA', (32, 32), color=(0, 0, 0, 0))
+                draw = ImageDraw.Draw(img)
+                draw.ellipse([2, 2, 30, 30], fill='#1E90FF')
+                try:
+                    font = ImageFont.truetype("arial.ttf", 20)
+                except:
+                    font = ImageFont.load_default()
+                text = "A"
+                text_bbox = draw.textbbox((0, 0), text, font=font)
+                text_width = text_bbox[2] - text_bbox[0]
+                text_height = text_bbox[3] - text_bbox[1]
+                x = (32 - text_width) // 2
+                y = (32 - text_height) // 2 - 2
+                draw.text((x, y), text, fill='white', font=font)
+                
+                temp_ico = "temp_settings_icon.ico"
+                img.save(temp_ico, format='ICO', sizes=[(32, 32)])
+                settings_window.iconbitmap(temp_ico)
+                os.remove(temp_ico)
+            except:
+                pass
+        
         # 노트북(탭) 생성
         notebook = ttk.Notebook(settings_window)
         notebook.pack(fill="both", expand=True, padx=10, pady=10)
